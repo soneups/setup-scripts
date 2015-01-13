fd3d939859de04cacf6734ca2b5957e65e549d03
Gary Sone <gary.sone@bh.sone.net>
2015-01-13 18:27:36 +0000

    Create SyncNTP.py
    Dont forget to run as SUDO, this python script will sync to a network based time server over NTP

---
+++ SyncNTP.py
@@ -0,0 +1,16 @@
+#HT - http://stackoverflow.com/questions/5222951/easy-way-to-get-the-correct-time-in-python                                                 
+#https://pypi.python.org/pypi/ntplib/                                                                                                       
+import time                                                                                                                                 
+import os                                                                                                                                   
+                                                                                                                                            
+try:                                                                                                                                        
+    import ntplib                                                                                                                           
+    client = ntplib.NTPClient()                                                                                                             
+    response = client.request('pool.ntp.org')                                                                                               
+    print client                                                                                                                            
+#    print 'date ' + time.strftime('%m%d%H%M%Y.%S',time.localtime(response.tx_time))                                                         
+    os.system('date ' + time.strftime('%m%d%H%M%Y.%S',time.localtime(response.tx_time)))                                                   
+except:                                                                                                                                     
+    print('Could not sync with time server.')                                                                                               
+                                                                                                                                            
+print('Done.') 
