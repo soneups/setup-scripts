619b5a72530c3ba3b70759df80879b28a2d0b0cd
Gary Sone <gary.sone@bh.sone.net>
2015-03-08 11:37:19 +0000

    Update SyncNTP.py
    added check to see if root

--- SyncNTP.py
+++ SyncNTP.py
@@ -1,8 +1,14 @@
 # this file - https://github.com/soneups/setup-scripts/raw/master/SyncNTP.py
 #HT - http://stackoverflow.com/questions/5222951/easy-way-to-get-the-correct-time-in-python                                                 
 #https://pypi.python.org/pypi/ntplib/                                                                                                       
+# added check to see if root
 import time                                                                                                                                 
-import os                                                                                                                                   
+import os
+
+# check and warn if not running with root permissions!
+if not 'SUDO_UID' in os.environ.keys():
+  print 'ERROR: Please re-run as root, hint - sudo'
+  sys.exit(1)
                                                                                                                                             
 try:                                                                                                                                        
     import ntplib                                                                                                                           
