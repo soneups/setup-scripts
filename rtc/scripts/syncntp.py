e867b73e05671637f030493db16420b2c95ea61f
Gary Sone <gary.sone@bh.sone.net>
2015-09-11 11:42:58 +0000

    Create syncntp.py

---
+++ rtc/scripts/syncntp.py
@@ -0,0 +1,24 @@
+# this file - https://github.com/soneups/setup-scripts/raw/master/SyncNTP.py
+#HT - http://stackoverflow.com/questions/5222951/easy-way-to-get-the-correct-time-in-python                                                 
+#https://pypi.python.org/pypi/ntplib/                                                                                                       
+# added check to see if root
+import time                                                                                                                                 
+import os
+import sys
+
+# check and warn if not running with root permissions!
+if not 'SUDO_UID' in os.environ.keys():
+  print 'ERROR: Please re-run as root, hint - sudo'
+  sys.exit(1)
+                                                                                                                                            
+try:                                                                                                                                        
+    import ntplib                                                                                                                           
+    client = ntplib.NTPClient()                                                                                                             
+    response = client.request('pool.ntp.org')                                                                                               
+    print client                                                                                                                            
+#    print 'date ' + time.strftime('%m%d%H%M%Y.%S',time.localtime(response.tx_time))                                                         
+    os.system('date ' + time.strftime('%m%d%H%M%Y.%S',time.localtime(response.tx_time)))                                                   
+except:                                                                                                                                     
+    print('Could not sync with time server.')                                                                                               
+                                                                                                                                            
+print('Done.') 
