0d544b010bb48dba460d8728b35308e959885798
soneups <gary.sone@bh.sone.net>
2019-03-18 17:46:18 +0000

    modified first run script

--- rtc/adafru_it.3295/first_run.sh
+++ rtc/adafru_it.3295/first_run.sh
@@ -1,1 +1,6 @@
-#!/bin/bash
+#!/bin/bash -v
+
+# adafuit.it/3295 - PCF8523 
+
+sudo i2cdetect -y 1
+# if i2cdetect retruns command  not found then install the set via 'sudo apt-get install python-smbus i2c-tools'
